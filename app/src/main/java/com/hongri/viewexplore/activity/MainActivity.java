ae446a1e318a4ca394a4f81809ab85d41f830af5
zhongyao <zhongyao@alibaba-inc.com>
2017-12-05 16:15:46 +0800

    ..

--- app/src/main/java/com/hongri/viewexplore/activity/MainActivity.java
+++ app/src/main/java/com/hongri/viewexplore/activity/MainActivity.java
@@ -1,13 +1,13 @@
-package view.hongri.com.viewexplore;
+package com.hongri.viewexplore.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(view.hongri.com.viewexplore.R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/hongri/viewexplore/activity/ViewEventActivity.java
@@ -0,0 +1,13 @@
+package com.hongri.viewexplore.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ViewEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(view.hongri.com.viewexplore.R.layout.activity_view_event);
+    }
+}
