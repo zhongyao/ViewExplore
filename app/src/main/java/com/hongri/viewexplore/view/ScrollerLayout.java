9eba2ba53b9e5b59d5f970f3dbd874108768d219
redsun.com <1285052668@qq.com>
2017-12-07 22:06:21 +0800

    ScrollerLayout

---
+++ app/src/main/java/com/hongri/viewexplore/view/ScrollerLayout.java
@@ -0,0 +1,132 @@
+package com.hongri.viewexplore.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.v4.view.ViewConfigurationCompat;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.Scroller;
+
+/**
+ * Created by zhongyao on 2017/12/7.
+ */
+
+public class ScrollerLayout extends ViewGroup {
+    private int leftBorder;
+    private int rightBorder;
+    private int mTouchSlop;
+    private Scroller mScroller;
+    private  int mXDown;
+    private int mXMove,mLastMove;
+    private int mLastX;
+    public ScrollerLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mScroller = new Scroller(context);
+        //获取TouchSlop值
+        mTouchSlop = ViewConfigurationCompat.getScaledPagingTouchSlop(ViewConfiguration.get(context));
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+           View childView = getChildAt(i);
+            //为ScrollerView中的每个子控件测量大小
+            measureChild(childView,widthMeasureSpec,heightMeasureSpec);
+        }
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (changed){
+            int childCount = getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View childView = getChildAt(i);
+                childView.layout(i*childView.getMeasuredWidth(),0,(i+1)*childView.getMeasuredWidth(),childView.getMeasuredHeight());
+            }
+            leftBorder = getChildAt(0).getLeft();
+            rightBorder = getChildAt(childCount - 1).getRight();
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                    mXDown = (int) ev.getRawX();
+                    mLastMove = mXDown;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                    mXMove = (int) ev.getRawX();
+                    mLastMove = mXMove;
+                    if (Math.abs(mXMove - mXDown) > mTouchSlop){
+                        return true;
+                    }
+
+                break;
+            case MotionEvent.ACTION_UP:
+
+                break;
+            default:
+                break;
+        }
+
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mXMove = (int) event.getRawX();
+                int mScrollX = mLastMove - mXMove;
+                if (getScaleX() + mScrollX < leftBorder){
+                    scrollTo(leftBorder,0);
+                    return true;
+                }else if (getScaleX() + mScrollX + getWidth()> rightBorder){
+                    scrollTo(rightBorder-getWidth(),0);
+                    return true;
+                }
+
+                scrollBy(mScrollX,0);
+                mLastMove = mXMove;
+                break;
+            case MotionEvent.ACTION_UP:
+                // 当手指抬起时，根据当前的滚动值来判定应该滚动到哪个子控件的界面
+                int targetIndex = (getScrollX() + getWidth() / 2) / getWidth();
+                int dx = targetIndex * getWidth() - getScrollX();
+                // 第二步，调用startScroll()方法来初始化滚动数据并刷新界面
+                mScroller.startScroll(getScrollX(), 0, dx, 0);
+                invalidate();
+                break;
+
+            default:
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void computeScroll() {
+//        super.computeScroll();
+        if (mScroller.computeScrollOffset()){
+            scrollTo(mScroller.getCurrX(),mScroller.getCurrY());
+            postInvalidate();
+        }
+    }
+}
